9b153f5fff5f31d579d3ef00363c9b136fb5bd9a
Arfaa Rashid <arfaamrashid@gmail.com>
2023-10-10 23:37:22 -0400

    Commented app.py

--- api/app.py
+++ api/app.py
@@ -2,6 +2,9 @@ import sqlite3
 from flask import Flask, render_template, request, url_for, flash, redirect
 from werkzeug.exceptions import abort
 
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'        #! FIXME: actually gen secretkey lol
+
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
@@ -15,9 +18,33 @@ def get_activity(activity_id):
         abort(404)
     return activity
 
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'        #! FIXME: actually gen secretkey lol
+#change "added" attribute of activity in DB depending on checkboxes
+@app.route('/mars_itinerary', methods =["GET", "POST"])
+def setAdded():
+    if request.method == "POST":
+        addedList = []
+        for i in range(1,4):                    #there are 3 activities for mars    ! TODO: get number of activities instead of hard-coding
+            found = request.form.get(str(i))    #get input from checkboxes form
+            if (found != None):
+                addedList.append(i)
 
+    conn = get_db_connection()
+    cur = conn.cursor()
+
+    for i in range(1,4):
+        if i in addedList:
+            cur.execute("UPDATE activities SET added=True WHERE id = ?", (i,))
+        else:
+            cur.execute("UPDATE activities SET added=False WHERE id = ?", (i,))         #if not selected, must reset value to false to wipe previous selections
+
+    activities = cur.execute('SELECT * FROM activities').fetchall()
+    conn.commit() 
+    conn.close()   
+
+    return render_template('itin.html', activities=activities)
+
+
+#functions to redirect to next page
 @app.route('/')
 def index():
     conn = get_db_connection()
@@ -44,7 +71,7 @@ def itin():
     conn = get_db_connection()
     activities = conn.execute('SELECT * FROM activities').fetchall()
     conn.close()
-    return render_template('itin.html',activities=activities)
+    return render_template('itin.html',activities=activities)               #! FIXME: setAdded() being used to direct to next page, not this (?). separate so this is used instead
 
 @app.route('/lastPage')
 def lastPage():
@@ -52,30 +79,3 @@ def lastPage():
     activities = conn.execute('SELECT * FROM activities').fetchall()
     conn.close()
     return render_template('lastPage.html',activities=activities)
-
-
-#set "added" attribute of activity to true after clicking checkbox
-@app.route('/mars_itinerary', methods =["GET", "POST"])
-def setAdded():
-    if request.method == "POST":
-        addedList = []
-        for i in range(1,4):            #there r 3 activities for mars
-            found = request.form.get(str(i))
-            if (found != None):
-                addedList.append(i)
-
-    conn = get_db_connection()
-    cur = conn.cursor()
-
-    for i in range(1,4):            #there are 3 activities for mars
-        if i in addedList:
-            cur.execute("UPDATE activities SET added=True WHERE id = ?", (i,))
-        else:
-            cur.execute("UPDATE activities SET added=False WHERE id = ?", (i,))
-
-    activities = cur.execute('SELECT * FROM activities').fetchall()
-
-    conn.commit() 
-    conn.close()   
-
-    return render_template('itin.html', activities=activities)    #change this to next html page name ?
